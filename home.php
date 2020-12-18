e9a780addc5d0528eacfa247e35725b039eddcd2
sciondev96 <sciondev96@gmail.com>
2020-12-18 12:58:41 +0530

    Initial Commit

---
+++ home.php
@@ -0,0 +1,39 @@
+<?php
+    include('./lib/db.php');
+?> 
+<html>
+<head>
+    <link rel="stylesheet" href="./css/main.css">
+    <title>
+        Game Management System
+    </title>
+</head>
+<body>
+    <div class="homediv">
+        <div class="homedivinner">
+            <div class="homexbox">
+                <div class="xboximg">
+                    <img src="./images/xboxlogo.jpg" width="300px" height="200px"/>
+                </div>
+                <div class="xboxlogin">
+                    <button class="xboxbtn">Login</button>
+                </div>
+                <div class="xboxregister">
+                    <button class="xboxbtn">Register</button>
+                </div>
+            </div>
+            <div class="homeps">
+                <div class="psimg">
+                    <img src="./images/pslogo.jpg" width="300px" height="200px"/>
+                </div>
+                <div class="pslogin">
+                    <button class="psbtn">Login</button>
+                </div>
+                <div class="psregister">
+                    <button class="psbtn">Register</button>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
+
---
+++ lib/db.php
@@ -0,0 +1,7 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '1458';
+    $db = 'gamedb';
+    $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+?>
