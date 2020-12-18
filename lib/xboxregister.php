749b8587569a5c3a3c360c415539dac1c899980b
sciondev96 <sciondev96@gmail.com>
2020-12-18 16:26:13 +0530

    xbox registration db done

---
+++ lib/xboxregister.php
@@ -0,0 +1,38 @@
+<?php
+    include('db.php');
+
+    $name = "";
+    $id = "";
+    $email = "";
+    $pass = "";
+    $ph = "";
+    $privacy = "";
+
+    $errors = array(); 
+
+    if($_POST['submit']) {
+        $name = mysqli_real_escape_string($mysqli, $_POST['xname']);
+        $id = mysqli_real_escape_string($mysqli, $_POST['xgamerid']);
+        $email = mysqli_real_escape_string($mysqli, $_POST['xemail']);
+        $pass = mysqli_real_escape_string($mysqli, $_POST['xpass']);
+        $ph = mysqli_real_escape_string($mysqli, $_POST['xph']);
+        $privacy = mysqli_real_escape_string($mysqli,$_POST['xchoice']);
+    
+    }
+
+    $user_check_query = "SELECT * FROM gamers WHERE gamerid='$id' LIMIT 1";
+    $result = mysqli_query($mysqli, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+    if ($user) { // if user exists
+        if ($user['gamerid'] === $id) {
+          array_push($errors, "Username already exists. Please choose a new one.");
+        }
+    }
+    if(count($errors) == 0) {
+        $query = "INSERT INTO gamers (gamerid, name, email, password, phnum, privacy) VALUES ('$id', '$name', '$email', '$pass', '$ph', '$privacy')"; 
+        mysqli_query($mysqli, $query);
+        header('location: ../home.html');
+    }
+
+    include('./error.php');
+?>
