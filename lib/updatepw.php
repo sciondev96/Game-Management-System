2913c39e6a13250b91de7cdcb6ef288f7a39d26a
sciondev96 <sciondev96@gmail.com>
2021-01-02 12:56:56 +0530

    session verification done

--- lib/updatepw.php
+++ lib/updatepw.php
@@ -3,14 +3,19 @@
     include('db.php');
 
     $pass = "";
-
-    if($_POST['submit']) {
-        $pass = mysqli_real_escape_string($mysqli, $_POST['pass1']);    
+    if(isset($_SESSION['regno'])) {
+        if($_POST['submit']) {
+            $pass = mysqli_real_escape_string($mysqli, $_POST['pass1']);    
+        }
+        $regno = $_SESSION['regno'];
+        $sql = "UPDATE gamers SET password='$pass' WHERE phnum='$regno'";
+        if($mysqli->query($sql)) {
+            header('location: ../home.html');
+        }
     }
-    $regno = $_SESSION['regno'];
-    $sql = "UPDATE gamers SET password='$pass' WHERE phnum='$regno'";
-    if($mysqli->query($sql)) {
-        header('location: ../home.html');
+    else {
+        header('location: ../forgot.html');
     }
+    
 
 ?>
